707011a02da49b6a5624b81fc0b52798e807e43d
Tino <valennvillar@gmail.com>
2023-10-02 00:34:53 -0300

    cree el animador para la caida de los caramelos, ya sea en el inicio del nivel o cuando ocurre una colision y se deben generar mas

---
+++ Candy Crush/GUI/Threads/AnimadorCaida.java
@@ -0,0 +1,46 @@
+package GUI.Threads;
+
+import javax.swing.*;
+import java.time.Duration;
+
+public class AnimadorCaida extends Thread{
+    protected JLabel jLabel;
+    protected int toX;
+    protected int toY;
+    protected int delay;
+
+    public AnimadorCaida(JLabel jLabel, int toX, int toY, int delay) {
+        this.jLabel = jLabel;
+        this.toX = toX;
+        this.toY = toY;
+        this.delay = delay;
+    }
+
+    public void run() {
+        int posX = jLabel.getX();
+        int posY = jLabel.getY();
+
+        boolean stopX = posX == toX;
+        boolean stopY = posY == toY;
+
+        try {
+            sleep(Duration.ofSeconds(4));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while(!stopX || !stopY) {
+            if(!stopX) posX += posX > toX ? -1 : 1;
+            if(!stopY) posY += posY > toY ? -1 : 1;
+            jLabel.setLocation(posX, posY);
+            stopX = posX == toX;
+            stopY = posY == toY;
+            try {
+                sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        jLabel = null;
+    }
+}
