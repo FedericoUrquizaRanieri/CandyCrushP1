f187a54bba82f579b76605c454859f9c34deaaff
Tino <valennvillar@gmail.com>
2023-10-02 00:38:35 -0300

    JLabel que se encarga de modelar un caramelo

--- Candy Crush/GUI/EntidadGrafica.java
+++ Candy Crush/GUI/EntidadGrafica.java
@@ -1,17 +1,23 @@
 package GUI;
 
-import javax.swing.JLabel;
+import Entidad.Entidad;
+import Entidad.Color;
 
+import javax.swing.*;
+import java.awt.*;
 
 public class EntidadGrafica extends JLabel{
-    //Atributos
-    //Constructor
-    public EntidadGrafica() {
+    private Entidad entidad;
+
+    public EntidadGrafica(int x, int y, int labelWidth, int labelHeight, Color color) {
+        ImageIcon ico = new ImageIcon("Candy Crush/Imagenes/Caramelos/"+ color.toString().toLowerCase() +".png");
+        Image img = ico.getImage();
+        Image new_img = img.getScaledInstance(labelWidth, labelHeight, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(new_img));
+        setBounds(x, y, labelWidth, labelHeight);
+        setOpaque(false);
     }
-    //Metodos
-    /**
-     * @return
-     */
+
     public void destruirse() {
     }
 }
